cae460b53583005fdf379b81c8d481e6ea1a5de7
fanjindai <fanchen4139@163.com>
2024-12-26 15:51:39 +0800

    feat: 构建调试参数工具函数调整

--- src/hooks/useLevaControls.ts
+++ src/hooks/useLevaControls.ts
@@ -9,49 +9,59 @@ type ControlsOptions = {
   folderSettings?: FolderSettings;
 };
 
-/**
- * 非调试模式下，提取 schema 的各项 value ，使其结构与 useControls 的返回值保持一致
- * @param string [key] - 控制面板 value 值实际对应的键
- * @param any [value] - 默认值
- * @param Object result - 默认参数对象
- */
-function schemaToValues(key, value, result) {
-  if (typeof value === "object" && "schema" in value) {
-    Object.entries(value.schema).forEach(([key, value]) => {
-      schemaToValues(key, value, result);
-    });
-  } else if (typeof value === "object" && "value" in value) {
-    result[key] = value.value;
-  } else {
-    result[key] = value;
-  }
-}
+// /**
+// * 非调试模式下，提取 schema 的各项 value ，使其结构与 useControls 的返回值保持一致
+// * @param string [key] - 控制面板 value 值实际对应的键
+// * @param any [value] - 默认值
+// * @param Object result - 默认参数对象
+// */
+// function schemaToValues(key, value, result) {
+//  if (typeof value === "object" && "schema" in value) {
+//    Object.entries(value.schema).forEach(([key, value]) => {
+//      schemaToValues(key, value, result);
+//    });
+//  } else if (typeof value === "object" && "value" in value) {
+//    result[key] = value.value;
+//  } else {
+//    result[key] = value;
+//  }
+// }
 
 /**
- * 根据 options.schema 构建默认参数，enableDebug 控制是否启用调试面板
- * @param Object [options] - useControls 函数的参数
- * @param string [options.name] - 文件夹名称
- * @param Schema [options.schema] - 描述控制面板中控件的结构和配置
- * @param FolderSettings [options?.folderSettings] - 文件夹设置
- * @param boolean enableDebug - 是否启用调试面板
- * @return 默认参数
+* 根据 options.schema 构建默认参数，enableDebug 控制是否启用调试面板
+* @param Object [options] - useControls 函数的参数
+* @param string [options.name] - 文件夹名称
+* @param Schema [options.schema] - 描述控制面板中控件的结构和配置
+* @param FolderSettings [options?.folderSettings] - 文件夹设置
+* @param boolean enableDebug - 是否启用调试面板
+* @return 默认参数
+
+/**
+ * 根据配置选项构建参数。
+ *
+ * @param {Object} options - 配置选项。
+ * @param {string} [options.name] - 文件夹名称（默认值：`undefined`）。
+ * @param {Schema} [options.schema] - 描述控制面板中控件的结构和配置。
+ * @param {Object} [options.folderSettings] - 文件夹设置。
+ * @param {boolean} [options.folderSettings.collapsed] - 是否折叠，默认值为 `true`。
+ * @param {function} [options.folderSettings.render] - 渲染函数，默认值为 `() => enableDebug`。
+ * @param {boolean} enableDebug - 是否启用调试面板。
+ * @returns {Object} 返回构建的默认参数。
  */
 export default function useLevaControls<S extends ControlsOptions>(
   options: S,
   enableDebug: boolean = false
 ): ExtractSchemaFromOptions<S> {
-  const { name, schema, folderSettings = { collapsed: true } } = options;
-  if (!enableDebug) {
-    return Object.entries(schema).reduce((pre, cur) => {
-      const [key, value] = cur;
-      schemaToValues(key, value, pre);
-      return pre;
-    }, {} as ExtractSchemaFromOptions<S>);
-  } else {
-    return useControls(
-      name,
-      schema,
-      folderSettings
-    ) as ExtractSchemaFromOptions<S>;
-  }
+  const { folderSettings = { collapsed: true, render: () => enableDebug } } =
+    options;
+
+  folderSettings.collapsed = folderSettings.collapsed ?? true;
+
+  folderSettings.render = folderSettings.render ?? (() => enableDebug);
+
+  return useControls(
+    options.name,
+    options.schema,
+    folderSettings
+  ) as ExtractSchemaFromOptions<S>;
 }

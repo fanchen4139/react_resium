b2596381a91c8c0d957cb4b5d9707fe71e1230ea
fanjindai <fanchen4139@163.com>
2024-12-26 15:53:27 +0800

    feat(types): 定义全局辅助类型

--- src/types/Common.ts
+++ src/types/Common.ts
@@ -1,3 +1,4 @@
+import type { Property } from "cesium";
 import type { ReactNode } from "react";
 
 export type WithChildren = {
@@ -11,3 +12,17 @@ export type DefaultControllerProps = {
 
 export type PartialWithout<T, K extends keyof T> = Pick<T, K> &
   Partial<Omit<T, K>>;
+
+export type RGBA = {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+};
+
+export type CesiumImage =
+  | Property
+  | string
+  | HTMLImageElement
+  | HTMLCanvasElement
+  | HTMLVideoElement;
